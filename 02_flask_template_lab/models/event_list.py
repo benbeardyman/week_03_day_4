cde99dbad7170714c4a3b0beac66957d63f713da
Ben Barlow <ben.barlow@yahoo.co.uk>
2022-12-08 18:30:04 +0000

    added boolean functionality

--- 02_flask_template_lab/models/event_list.py
+++ 02_flask_template_lab/models/event_list.py
@@ -1,9 +1,9 @@
 from models.event import *
 import datetime
 
-event1 = Event(datetime.date(2023, 5,25), "Jessica", 6, "Outside Bar", "Birthday party")
-event2 = Event(datetime.date(2023, 3, 7), "Alvin", 12, "Outside Bar", "Birthday party")
-event3 = Event(datetime.date(2023, 1, 5), "Amy + Ben", 16, "Dining Room", "Wedding aniversary")
+event1 = Event(datetime.date(2023, 5,25), "Jessica", 6, "Outside Bar", "Birthday party", True)
+event2 = Event(datetime.date(2023, 3, 7), "Alvin", 12, "Outside Bar", "Birthday party", True)
+event3 = Event(datetime.date(2023, 1, 5), "Amy + Ben", 16, "Dining Room", "Wedding anniversary", True)
 
 events = [event1, event2, event3]
 
